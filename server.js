11489092275052c158a42778f8f974ff165ca4b4
Esteban Donis <estebandonis10@gmail.com>
2024-02-20 19:08:50 -0600

    Cambios a nuevo link

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const userRoutes = require("./src/routes/user.js");
 const teamRoutes = require("./src/routes/team.js");
 const playerRoutes = require("./src/routes/player.js");
 const gameRoutes = require("./src/routes/game.js");
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 //Routes
 app.get("/", (req, res) => {
