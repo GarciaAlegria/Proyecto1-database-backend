2b1d8636cbadbfc8c7a46f878979869506963ec9
GarciaAlegria <abnerivan2003@gmail.com>
2024-02-22 11:50:20 -0600

    cambios a team

--- src/routes/team.js
+++ src/routes/team.js
@@ -20,9 +20,9 @@ router.post("/teams/create", (req, res) => {
 });
 
 router.get("/teams/find", (req, res) => {
-  const { name } = req.body;
+  const { name } = req.query; 
   teamSchema
-    .find({ name: name })
+    .find({ name: { $regex: new RegExp(name, 'i') } }) 
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
@@ -37,19 +37,19 @@ router.put("/teams/update/:id", (req, res) => {
 });
 
 
-router.delete("/teams/deleteByName/:name", (req, res) => {
-  const { name } = req.params;
+router.delete("/teams/delete/:id", (req, res) => {
+  const { id } = req.params;
   teamSchema
-    .findOneAndRemove({ name: name })
-    .then((data) => {
-      if (data) {
-        res.json({ message: "Equipo eliminado correctamente" });
-      } else {
-        res.json({ message: "No se encontró el equipo con ese nombre" });
-      }
+    .findByIdAndDelete(id)
+    .then(() => {
+      res.json({ message: "Equipo eliminado correctamente" });
     })
-    .catch((error) => res.json({ message: error }));
+    .catch((error) => {
+      console.log("Error deleting team:", error); // Agregar esta línea para ver el error en la consola del servidor
+      res.status(500).json({ message: "Error interno del servidor" });
+    });
 });
+
 
 
 module.exports = router;
