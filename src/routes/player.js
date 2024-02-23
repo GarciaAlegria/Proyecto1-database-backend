9fa1a3849ab414712feff2954befec68a3fab8d7
Esteban Donis <estebandonis10@gmail.com>
2024-02-22 23:08:06 -0600

    Update players agregado

--- src/routes/player.js
+++ src/routes/player.js
@@ -22,8 +22,6 @@ router.post("/players/create", async (req, res) => {
 
   const newStats = req.body.stats.split(",").map(Number);
 
-  console.log(newStats);
-
   const { stats, team, ...restBody } = req.body;
 
   const playerData = {
@@ -40,9 +38,6 @@ router.post("/players/create", async (req, res) => {
 });
 
 router.post("/players/delete", (req, res) => {
-  console.log(req.body);
-  console.log(req.body.name.first_name);
-  console.log(req.body.name.last_name);
   playerSchema
     .deleteOne({
       name: {
@@ -58,6 +53,39 @@ router.post("/players/delete", (req, res) => {
       }
     })
     .catch((error) => res.json({ message: error.message }));
+});
+
+router.put("/players/update", async (req, res) => {
+  const teamPlayer = await teamSchema.findOne({ name: req.body.team });
+  if (!teamPlayer) {
+    return res.status(400).send("Equipo no encontrado");
+  }
+
+  const { team, beforeName, ...restBody } = req.body;
+
+  const playerData = {
+    ...restBody,
+    team: teamPlayer._id,
+  };
+
+  const updateObject = {};
+  // Update specific fields if needed
+  updateObject.name = playerData.name;
+  updateObject.position = playerData.position;
+  updateObject.height = playerData.height;
+  updateObject.weight_pounds = playerData.weight_pounds;
+  updateObject.stats = playerData.stats;
+  updateObject.team_id = playerData.team;
+
+  // Update using the player's _id
+  await playerSchema
+    .findOneAndUpdate(
+      { name: beforeName }, // Match using the unique identifier
+      { $set: updateObject },
+      { new: true }, // Return the updated document
+    )
+    .then(() => res.json({ message: "Player actualizado exitosamente" }))
+    .catch((error) => res.json({ message: error }));
 });
 
 module.exports = router;
